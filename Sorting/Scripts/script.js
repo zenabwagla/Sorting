fe8fc7b328034e3df19ba4ea799f860d1aeb5946
Zenab WaglaWala <Zenab.Waglawala@Advaiya.com>
2021-08-03 11:28:12 +0530

    Add project files.

---
+++ Sorting/Scripts/script.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular.module("MyModule", []).controller("MyController", function ($scope) {
+    var employees = [
+        { name: 'Zenab', dateOfBirth: new Date("June 13, 1998"), gender: "Female", salary: 50000 },
+        { name: 'abc', dateOfBirth: new Date("June 18, 1998"), gender: "Male", salary: 55000 },
+        { name: 'xyz', dateOfBirth: new Date("June 30, 1998"), gender: "Male", salary: 30000 },
+        { name: 'dfg', dateOfBirth: new Date("July 13, 1998"), gender: "Female", salary: 560000 }
+    ];
+
+    $scope.employees = employees;
+    $scope.sortColumn = "name";
+    $scope.reverseSort = false;
+
+    $scope.sortData = function (column) {
+        $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+        $scope.sortColumn = column;
+    }
+
+    $scope.getSortClass = function (column) {
+        if ($scope.sortColumn == column) {
+            return $scope.reverseSort ? '.arrow_down' : '.arrow_up';
+        }
+        else {
+            return '';
+        }
+    }
+})
